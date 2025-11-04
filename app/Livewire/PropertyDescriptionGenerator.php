9bf00654ad061a34cb846a403592813673298148
Adedayo Balogun <adedayobalogun94@gmail.com>
2025-11-04 15:07:07 +0100

    feat: create PropertyDescriptionGenerator Livewire component with validation

---
+++ app/Livewire/PropertyDescriptionGenerator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PropertyDescriptionGenerator extends Component
+{
+    public function render()
+    {
+        return view('livewire.property-description-generator');
+    }
+}
